85a30cf67829ad366690bbaab76ee5a85c9bc918
yassine chahid <chahidyassine14@gmail.com>
2026-03-01 18:13:48 +0000

    remove items

--- foodyass/src/app/layout.tsx
+++ foodyass/src/app/layout.tsx
@@ -5,8 +5,14 @@ import { LocaleLayoutProps } from "@/types/children";
 import ClientRoot from "@/components/ClientRoot";
 
 export const metadata: Metadata = {
-  title: "food",
+  title: "Foody",
   description: "food, la solution innovante pour faciliter l'accès aux soins !",
+  icons: {
+    icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon.ico", sizes: "any" },
+    ],
+  },
 };
 
 export default async function LocaleLayout({ children }: LocaleLayoutProps) {
